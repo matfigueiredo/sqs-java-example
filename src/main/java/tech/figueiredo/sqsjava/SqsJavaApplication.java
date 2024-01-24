0829532e679c5383368c42c887611c907be84251
Matheus Figueiredo <figo.ppv@gmail.com>
2024-01-24 10:30:16 -0300

    fix: remove unused imports

--- src/main/java/tech/figueiredo/sqsjava/SqsJavaApplication.java
+++ src/main/java/tech/figueiredo/sqsjava/SqsJavaApplication.java
@@ -1,12 +1,8 @@
 package tech.figueiredo.sqsjava;
 
-import io.awspring.cloud.sqs.operations.SqsTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import tech.figueiredo.sqsjava.consumer.MyMessage;
+
 
 @SpringBootApplication
 public class SqsJavaApplication {
